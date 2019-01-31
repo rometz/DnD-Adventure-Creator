43f7280972b6173dd156a378cb8595c5333cc671
Rollin Metzger <alcesalces@github.com>
2019-01-30 18:07:45 -0600

    Create specific routes for sessions path

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static#home'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  # get auth/twitch/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
 end
