293dd75e6e3aa0430be0f56b6b6451c9fe009c69
Rollin Metzger <alcesalces@github.com>
2019-02-06 18:22:49 -0600

    Add validations and relationships to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :characters
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
 end
