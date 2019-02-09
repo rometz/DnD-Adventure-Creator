a8f9d7b0d910d0036882a6f9bb66ed28bef0f303
Rollin Metzger <alcesalces@github.com>
2019-02-09 12:36:51 -0600

    Fill out Quests controller

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -5,28 +5,48 @@ class QuestsController < ApplicationController
     end
 
     def show
+        # for JS framework
+        @id = params[:id]
+
+        @quest = Quest.find_by(id: params[:id])
     end
     
     def new
+        @quest = Quest.new
     end
 
     def create
+        @quest = Quest.new(quests_params)
+        @quest.user_id = current_user.id if current_user
+        if @quest.save
+            redirect_to quests_path(@quest)
+        else
+            render 'new'
+        end
     end
 
     def edit
+        #for JS framework
+        @id = params[:id]
+
+        @quest = Quest.find_by(id: params[:id])
     end
 
     def update
+        @quest = Quest.find_by(id: params[:id])
+        @quest.update(quests_params)
+        redirect_to quests_path(@quest)
     end
 
     def destroy
+        @quest = Quest.find_by(id: params[:id])
+        @quest.destroy
+        redirect_to quests_path(@quest)
     end
 
     private
 
     def quests_params
-    end
-
-    def get_user
+        params.require(:quest).permit(:name, :description)
     end
 end
