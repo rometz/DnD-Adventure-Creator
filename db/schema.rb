1446ab468ec26441458016c6c6754dc9dde63439
Rollin Metzger <alcesalces@github.com>
2019-02-09 13:09:54 -0600

    Migrate Quests table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_235227) do
+ActiveRecord::Schema.define(version: 2019_02_09_183927) do
+
+  create_table "quests", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
