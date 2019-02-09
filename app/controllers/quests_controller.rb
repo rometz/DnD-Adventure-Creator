f0690c72079a44b08b1259b13141a83783b2d909
Rollin Metzger <alcesalces@github.com>
2019-02-09 12:22:33 -0600

    Create Quests skeleton MVC

---
+++ app/controllers/quests_controller.rb
@@ -0,0 +1,32 @@
+class QuestsController < ApplicationController
+    before_action :logged_in?
+
+    def index
+    end
+
+    def show
+    end
+    
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def quests_params
+    end
+
+    def get_user
+    end
+end
