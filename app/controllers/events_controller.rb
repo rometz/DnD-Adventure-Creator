c9f71ad9c32e5f65610d3960ee8682fcd9997013
Rollin Metzger <alcesalces@github.com>
2019-02-16 13:41:51 -0600

    Add #index, #show, #new, #create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,15 +2,28 @@ class EventsController < ApplicationController
     before_action :logged_in?
 
     def index
+        @events = Event.where(quest_id: params[:quest_id])
+        @quest = Quest.find_by(id: params[:quest_id])
     end
 
     def show
+        @event = Event.find_by(id: params[:id])
+        @quest = Quest.find_by(id: params[:budget_id])
     end
 
     def new
+        @event = Event.new
+        @quest = Quest.find_by(id: params[:quest_id])
     end
 
     def create
+        @event = Event.new(event_params)
+        @quest = Quest.find_by(id: params[:event][:quest_id])
+        if @event.save
+            redirect_ti events_path(@quest, :quest_id => params[:event][:quest_id])
+        else
+            render 'new'
+        end
     end
 
     def edit
