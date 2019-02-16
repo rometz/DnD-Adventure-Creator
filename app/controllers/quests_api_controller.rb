e36e6b15d55b4108b741b8baf3a10b8805b46ff4
Rollin Metzger <alcesalces@github.com>
2019-02-15 20:51:16 -0600

    Add QuestsApi #index & #show

--- app/controllers/quests_api_controller.rb
+++ app/controllers/quests_api_controller.rb
@@ -1,9 +1,13 @@
 class QuestsApiController < ApplicationController
 
     def index
+        quests = Quest.where(user_id: current_user.id)
+        render json: quests
     end
 
     def show
+        quest = Quest.find_by(id: params[:id])
+        render json: quest.to_json(:methods => <<input method not yet created>> )
     end
 
     def new
