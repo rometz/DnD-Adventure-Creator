7d152aaed549eba4afdfcfda46ffdc71dd6ad87e
Rollin Metzger <alcesalces@github.com>
2019-02-16 13:15:32 -0600

    Outline EventsController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,30 @@
 class EventsController < ApplicationController
+    before_action :logged_in?
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def events_params
+        params.require(:events).permit(:name, :description, :combat, :social, :search, :quest_id)
+    end
 end
