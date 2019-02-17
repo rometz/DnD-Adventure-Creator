9e540b870d068fa9c5e34c07d526920cad232cf3
Rollin Metzger <alcesalces@github.com>
2019-02-17 17:40:25 -0600

    Add Event JS constructor

---
+++ app/assets/javascripts/event.js
@@ -0,0 +1,9 @@
+class Event {
+    constructor(name, description, combat, social, search) {
+        this.name = name;
+        this.description = description;
+        this.combat = combat;
+        this.social = social;
+        this.search = search;
+    };
+}
