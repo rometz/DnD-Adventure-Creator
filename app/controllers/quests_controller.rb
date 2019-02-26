4e299fc9038c51db5f34c131a7f72f94bda5fce4
Rollin Metzger <alcesalces@github.com>
2019-02-25 20:52:51 -0600

    Remove comment

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -2,7 +2,7 @@ class QuestsController < ApplicationController
     before_action :logged_in?
 
     def index
-        @user = User.find_by(id: current_user.id)
+        @user = User.find_by(id: current_user.id)aa
     end
 
     def show
