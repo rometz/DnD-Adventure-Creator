ae54216ad6880d510eded6919dd986a8264280a4
Rollin Metzger <alcesalces@github.com>
2019-03-24 18:49:39 -0500

    Remove unnecessary method code

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,11 +21,6 @@ class User < ApplicationRecord
             Quest.where(user_id: @user.id).find_each do |quest|
                 @quests << quest
             end
-            if @quests == nil
-                @quest = 0
-            else
-                @quest = @quests[0]
-            end
 
             @events = []
             Event.where(quest_id: @quest_id).find_each do |e|
