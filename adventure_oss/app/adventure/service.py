458e45ab36e0692b3073c64c7d7cafcfabef5748
Rollin Metzger <rollin.metzger@gmail.com>
2019-10-21 19:19:17 -0500

    Finish Service class for the adv repo

--- adventure_oss/app/adventure/service.py
+++ adventure_oss/app/adventure/service.py
@@ -3,20 +3,36 @@ from ..repository.mongo import MongoRepository
 from .schema import AdventureSchema
 
 class Service(object):
-    def __init__(self, user_id, repo_client=Repository(adapter=MongoRepository)):
-        self.repo_client = repo_client
+    def __init__(self, user_id, adv_client=Repository(adapter=MongoRepository)):
+        self.adv_client = adv_client
         self.user_id = user_id
 
         if not user_id:
             raise Exception("user id not provided")
 
     def find_all_adventures(self):
-        adventures = self.repo_client.find_all({'user_id': self.user_id})
+        adventures = self.adv_client.find_all({'user_id': self.user_id})
         return [self.dump(adventure) for adventure in adventures]
 
-    def find_adventure(self, repo_id):
-        adventure = self.repo_client.find({'user_id': self.user_id})
+    def find_adventure(self, adventure_id):
+        adventure = self.adv_client.find({'user_id': self.user_id, 'adventure_id': adventure_id})
 
     def create_adventure_for(self, advRepo):
-        self.repo_client.create(self.prepare_adventure(advRepo))
+        self.adv_client.create(self.prepare_adventure(advRepo))
         return self.dump(advRepo.data)
+
+    def update_adventure_with(self, adventure_id, advRepo):
+        records_affected = self.adv_client.update({'user_id': self.user_id, 'adventure_id': adventure_id}, self.prepare_adventure(advRepo))
+        return records_affected > 0
+
+    def delete_adventure_with(self, adventure_id):
+        records_affected = self.adv_client.delete({'user_id': self.user_id, 'adventure_id': adventure_id})
+        return records_affected > 0
+
+    def dump(self, data):
+        return AdventureSchema(exclude=['_id']).dump(data).data
+
+    def prepare_adventure(self, advRepo):
+        data = advRepo.data
+        data['user_id'] = self.user_id
+        return data
