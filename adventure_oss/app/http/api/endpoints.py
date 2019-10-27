705da83e39451f1a294217679a6134793ae7e058
Rollin Metzger <rollin.metzger@gmail.com>
2019-10-27 18:20:08 -0500

    Create endpoint for create()

--- adventure_oss/app/http/api/endpoints.py
+++ adventure_oss/app/http/api/endpoints.py
@@ -12,3 +12,16 @@ CORS(app)
 def index():
     return json_response(Adventure(g.user).find_all_adventures())
 
+
+@app.route("/adventures", methods=["POST"])
+@login_required
+def create():
+    adventure_repo = AdventureSchema().load(json.loads(request.data))
+
+    if adventure_repo.errors:
+        return json_response({'error': adventure_repo.errors}, 422)
+
+    adv = Adventure(g.user).create_adventure_for(adventure_repo)
+    return json_response(adv)
+
+
