6645019684bae1d698349eabff015ca5d7e0ab3e
Rollin Metzger <rollin.metzger@gmail.com>
2019-10-30 19:54:42 -0500

    Refactor PrivateRoute to use Context

--- adventure_oss/app/http/web/app/src/PrivateRoute.js
+++ adventure_oss/app/http/web/app/src/PrivateRoute.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
+import { useAuth } from "./context/auth";
 
 function PrivateRoute({ component: Component, ...rest }) {
+    const isAuthenticated = useAuth();
 
     return(
-        <Route {...rest} render={(props) => (
-            <Component {...props} />
-        )}
+        <Route
+            {...rest}
+            render={(props) =>
+                isAuthenticated ? (
+                <Component {...props} />
+                ) : (
+                <Redirect to="/" />
+            )}
         />
     );
 }
