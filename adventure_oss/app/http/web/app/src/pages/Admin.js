5f9c64c80ba6246edb9dfc771c57e38e5582352c
Rollin Metzger <rollin.metzger@gmail.com>
2019-11-03 09:30:24 -0600

    Update Admin.js to allow user logout and initial auth handle

--- adventure_oss/app/http/web/app/src/pages/Admin.js
+++ adventure_oss/app/http/web/app/src/pages/Admin.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Button } from '../components/AuthForm';
+import { useAuth } from '../context/auth';
 
 function Admin(props) {
-    return <div>Admin Page</div>
+    const { setAuthTokens } = useAuth();
+
+    function logOut() {
+        setAuthTokens();
+    }
+
+    return (
+        <div>
+            <div>Admin Page</div>
+            <Button onClick={logOut}>log out</Button>
+        </div>
+    );
 }
 
 export default Admin;
